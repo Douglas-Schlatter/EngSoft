102585454997fb0f38ef9403425f795ddf829688
luanahahn <00315418@ufrgs.br>
2023-08-20 21:26:37 -0300

    add interfaces usuario

--- src/view/TelaInicial.java
+++ src/view/TelaInicial.java
@@ -39,7 +39,6 @@ public class TelaInicial extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle(" ");
-        setPreferredSize(new java.awt.Dimension(1077, 501));
 
         jLabel2.setFont(new java.awt.Font("Trebuchet MS", 0, 36)); // NOI18N
         jLabel2.setText("Sistema RU++");
@@ -98,7 +97,7 @@ public class TelaInicial extends javax.swing.JFrame {
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(308, 308, 308)
+                        .addGap(296, 296, 296)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
